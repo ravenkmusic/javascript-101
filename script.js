2901f2313a70e7459c123c95135c03ea920f263a
Raven Hendricks <ravenkbeats@gmail.com>
2023-08-31 18:42:33 -0400

    edited js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
-const buttondBc = document.querySelector(".btn-bc");
-const buttonTitle = document.querySelector("#btn-bc");
-const buttonCircles = document.querySelector("#btn-circles");
+const buttondBc = document.querySelector(".btn-bc")
+const buttonTitle = document.querySelector("#btn-bc")
+const buttonCircles = document.querySelector("#btn-circles")
+
+console.log({buttondBc, buttonTitle, buttonCircles})
