b4c1b6f82f16588ce9765e3384accbc1e73086f1
Raven K Hendricks <85319728+ravenkmusic@users.noreply.github.com>
2023-08-31 18:41:18 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+const buttondBc = document.querySelector(".btn-bc")
+const buttonTitle = document.querySelector("#btn-bc")
+const buttonCircles = document.querySelector("#btn-circles")
+
+console.log({buttondBc, buttonTitle, buttonCircles})
