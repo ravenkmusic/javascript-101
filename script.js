0657c81c8d873cd60653125e8b5b31698b8283ee
Raven K Hendricks <85319728+ravenkmusic@users.noreply.github.com>
2023-08-31 19:11:56 -0400

    Update script.js
    added buttons and event listener to change background color

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const buttondBc = document.querySelector(".btn-bc")
 const buttonTitle = document.querySelector("#btn-bc")
 const buttonCircles = document.querySelector("#btn-circles")
+const body = document.querySelector("body");
+
+body.style.backgroundColor = "green";
 
 console.log({buttondBc, buttonTitle, buttonCircles})
+
+buttondBc.addEventListener('click',()=>{
+    body.style.backgroundColor = "yellow";
+})
